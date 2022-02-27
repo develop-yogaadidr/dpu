8a29b4cf4f38a5f48fa46c6ba9d958f536c08f6c
Yoga Adi Dharma <develop.yogaadidr@gmail.com>
2022-02-27 19:10:09 +0700

    feat: install all needed node dependencies

--- App.js
+++ App.js
@@ -3,110 +3,176 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow strict-local
+ * @flow
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  View,Text
 } from 'react-native';
+// import {AsyncStorage, Alert, Platform} from 'react-native';
+// import firebase from 'react-native-firebase';
+// import {Actions} from 'react-native-router-flux';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+ import Routes from './Routes'
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+export default class App extends React.Component {
+  async componentDidMount() {
+    this.checkPermission();
+    // this.createNotificationListeners();
+  }
+  componentWillUnmount() {
+    // this.notificationListener();
+    // this.notificationOpenedListener();
+  }
+  async createNotificationListeners() {
+    const channel = new firebase.notifications.Android.Channel(
+      'test-channel',
+      'Test Channel',
+      firebase.notifications.Android.Importance.Max,
+    ).setDescription('My apps test channel');
+
+    // Create the channel
+    firebase.notifications().android.createChannel(channel);
+
+    this.notificationListener = firebase
+      .notifications()
+      .onNotification(async notification => {
+        const {title, body, data} = notification;
+
+        // this.showAlert(title, body);
+        if (Platform.OS === 'android') {
+          const localNotification = new firebase.notifications.Notification({
+            sound: 'default',
+            show_in_foreground: true,
+          })
+            .setNotificationId(notification.notificationId)
+            .setTitle(notification.title)
+            .setBody(notification.body)
+            .setData(notification.data)
+            .android.setChannelId('test-channel') // e.g. the id you chose above
+            //.android.setSmallIcon('ic_launcher') // create this icon in Android Studio
+            .android.setColor('#000000') // you can set a color here
+            .android.setPriority(firebase.notifications.Android.Priority.High);
+
+          firebase
+            .notifications()
+            .displayNotification(localNotification)
+            .catch(err => console.error(err));
+        }
+        console.log('hoeforegorund');
+        // await this.updateLocalData(data)
+      });
+
+    /*
+     * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+     * */
+    this.notificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened(notificationOpen => {
+        const {title, body, data} = notificationOpen.notification;
+
+        console.log('bg', data);
+
+        /*
+             if(notif == null){
+               AsyncStorage.setItem('notification', JSON.stringify(data));
+             }else{
+               
+               AsyncStorage.setItem('notification', JSON.stringify(newnotif));
+ 
+             }*/
+
+        Actions.notificationDetail({
+          dataNotification: JSON.parse(data.message),
+        });
+        console.log('onNotificationOpened');
+      });
+
+    /*
+     * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+     * */
+    const notificationOpen = await firebase
+      .notifications()
+      .getInitialNotification()
+      .catch();
+    if (notificationOpen) {
+      const {title, body, data} = notificationOpen.notification;
+      console.log('closed', data);
+
+      Actions.notificationDetail({dataNotification: JSON.parse(data.message)});
+
+      //Actions.tripDetail({tripId: tripId});
+      console.log(notificationOpen.notification);
+
+      // Actions.announcement()
+    }
+    /*
+     * Triggered for data only payload in foreground and background (content_available: true , fcm-node)
+     * */
+    this.messageListener = firebase.messaging().onMessage(async message => {
+      console.log('donlu', message);
+      await this.updateLocalData(message._data);
+    });
+  }
+  async updateLocalData(data) {
+    // let dataNotification = await AsyncStorage.getItem('notification');
+    // if (dataNotification === null || dataNotification === undefined) {
+    //   let newnotif = [];
+    //   let datam = JSON.parse(data.message);
+    //   newnotif.push(datam);
+    //   console.log(JSON.stringify(newnotif));
+    //   await AsyncStorage.setItem('notification', JSON.stringify(newnotif));
+    // } else {
+    //   let newnotif = JSON.parse(dataNotification);
+    //   console.log(newnotif);
+    //   let datam = JSON.parse(data.message);
+    //   newnotif.push(datam);
+
+    //   console.log('isi', JSON.stringify(newnotif));
+    //   await AsyncStorage.setItem('notification', JSON.stringify(newnotif));
+    // }
+  }
+  async checkPermission() {
+    // const enabled = await firebase.messaging().hasPermission().catch();
+    // if (enabled) {
+    //   // this.getToken();
+    // } else {
+    //   // this.requestPermission();
+    // }
+  }
+  async requestPermission() {
+    // try {
+    //   await firebase.messaging().requestPermission();
+    //   // User has authorised
+    //   this.getToken();
+    // } catch (error) {
+    //   // User has rejected permissions
+    //   console.log('permission rejected');
+    // }
+  }
+  async getToken() {
+    // try {
+    //   let fcmToken = await AsyncStorage.getItem('fcmToken');
+
+    //   //AsyncStorage.getItem('token', (error, result) => {
+    //   if (fcmToken === undefined || fcmToken === null) {
+    //     fcmToken = await firebase.messaging().getToken();
+    //     console.log(fcmToken);
+    //     if (fcmToken) {
+    //       // user has a device token
+    //       await AsyncStorage.setItem('fcmToken', fcmToken);
+    //     }
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }
+  render() {
+    return <View>
+      <Text>
+        RUN
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    </View>;
+  }
+}
---
+++ Routes.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import { Router, Scene, Stack, Lightbox, Drawer, Tabs } from 'react-native-router-flux'
+import {AsyncStorage} from 'react-native';
+
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+// import reducers from './src/reducers'
+
+// import Login from './src/components/Login'
+// import Register from './src/components/Register'
+// import ReportUser from './src/components/ReportUser'
+
+// import HomeUser from './src/components/HomeUser'
+// import HomeAdmin from './src/components/HomeAdmin'
+// import FirstLoad from './src/components/FirstLoad'
+// import ProgressReport from './src/components/ProgressReport'
+// import DetailReportUser from './src/components/DetailReportUser'
+// import DetailReportAdmin from './src/components/DetailReportAdmin'
+// import NotificationList from './src/components/NotificationList'
+// import NotificationDetail from './src/components/NotificationDetail'
+
+// import Faq from './src/components/Faq'
+// import Information from './src/components/Information'
+// import Contact from './src/components/Contact'
+
+// import ShareApp from './src/components/ShareApp'
+
+// import DrawerMenu from './src/components/DrawerMenu'
+
+const store = createStore(
+  // reducers,
+  applyMiddleware(thunk)
+)
+
+ 
+export default class Routes extends React.Component{
+   constructor(props){
+      super(props)
+      this.state = {
+         hasToken: false
+      }
+   }
+ async componentDidMount(){
+    //  let token = AsyncStorage.getItem('token')
+     console.log(token)
+    //  if(token !== null || token===''){
+    //     this.setState({ hasToken: true})
+    //   }else{
+    //     this.setState({ hasToken: false})
+    //   }
+      
+   }
+render(){
+    return(
+      <View>
+      <Text>
+        aksjdhaskdh
+      </Text>
+    </View>
+    // <Provider store={store}>
+    // <Router >
+    //      <Scene>
+    //         <Scene key="firstLoad" component={FirstLoad} hideNavBar initial = {true}/>
+    //         <Scene
+    //             key="login"
+    //             component={Login}
+    //             type="reset"
+    //             //initial = {true}
+    //             hideNavBar
+    //         />
+    //         <Drawer
+    //             hideNavBar
+    //             key="drawerMenu"
+    //             contentComponent={DrawerMenu}
+    //             drawerWidth={250}
+    //             drawerPosition="left"
+    //             //nitial = {true}
+
+    //             style={{backgroundColor:"#1E1E1E"}}
+    //         >
+    //             <Scene key="homeUser" component={HomeUser} hideNavBar/>
+    //             <Scene key = "faq" component={Faq}  hideNavBar/>
+    //             <Scene key = "contact" component={Contact}  hideNavBar/>
+    //             <Scene key = "information" component={Information}  hideNavBar/>
+              
+               
+
+    //         </Drawer>
+    //         <Drawer
+    //             hideNavBar
+    //             key="drawerMenuAdmin"
+    //             contentComponent={DrawerMenu}
+    //             drawerWidth={250}
+    //             drawerPosition="left"
+    //             style={{backgroundColor:"#1E1E1E"}}
+    //         >
+    //           <Scene key="homeAdmin" component={HomeAdmin} hideNavBar/>
+    //           <Scene key = "faq" component={Faq}  hideNavBar/>
+    //           <Scene key = "contact" component={Contact}  hideNavBar/>
+    //           <Scene key = "information" component={Information}  hideNavBar/>
+
+              
+    //         </Drawer>
+    //         <Scene key = "reportUser" component={ReportUser}  hideNavBar/>
+    //         <Scene key = "progressReport" component={ProgressReport}  hideNavBar/>
+    //         <Scene key = "detailReportUser" component={DetailReportUser}  hideNavBar/>
+    //         <Scene key = "detailReportAdmin" component={DetailReportAdmin}  hideNavBar/>
+    //         <Scene key = "notificationList" component={NotificationList}  hideNavBar/>
+    //         <Scene key = "notificationDetail" component={NotificationDetail}  hideNavBar/>
+    //         <Scene key = "shareApp" component={ShareApp}  hideNavBar/>
+    //         <Scene key = "register" component={Register}  hideNavBar/>
+           
+
+    //     </Scene>
+    // </Router>
+    // </Provider>
+ )
+}
+}
